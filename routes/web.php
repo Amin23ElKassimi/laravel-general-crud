f2f650910c401c1db47ace638897eb665010737b
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-02-08 17:20:31 +0100

    - Created Controller - Defined Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\Guest\PokemonController as GuestPokemonController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+
+
+Route::get('/pokemons', [GuestPokemonController::class, 'index'])->name('guest.pokemons.index');
+Route::post('/pokemons', [GuestPokemonController::class, 'store'])->name('guest.pokemons.store');
+Route::get('/pokemons/create', [GuestPokemonController::class, 'create'])->name('guest.pokemons.create');
+Route::get('/pokemons/{pokemon}', [GuestPokemonController::class, 'show'])->name('guest.pokemons.show');
+Route::get('/pokemons/{pokemon}/edit', [GuestPokemonController::class, 'edit'])->name('guest.pokemons.edit');
+Route::put('/pokemons/{pokemon}', [GuestPokemonController::class, 'update'])->name('guest.pokemons.update');
+
