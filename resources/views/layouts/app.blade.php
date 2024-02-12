574b444d3490df26631bf24368f3659b707af831
UmbertoSerino <serumberto@gmail.com>
2024-02-12 22:50:06 +0100

    Pokemon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="{{ Vite::asset('resources/assets/img/images.png') }}" type="image/x-icon">
-    <title>I pochemones</title>
+    <title>Pokemon
+    </title>
     
     <!-- Includiamo gli assets con la direttiva vite -->
     @vite('resources/js/app.js')
